d455a5146073e7f225d6dd46971a9a186a7d16ec
Vlad A Chiriac <vladc.@vlads-mbp.home>
2022-04-14 10:31:53 +0100

    Update failing tests

--- test/services/ProductServiceSpec.scala
+++ test/services/ProductServiceSpec.scala
@@ -10,7 +10,7 @@ class ProductServiceSpec extends AnyFlatSpec with Matchers {
   "ProductService" should
     "get the list of products" in {
       val productService = new ProductService
-      productService.getList shouldBe List(Product("bike", 100, "Red bike", 50), Product("car", 2000, "Nice car", 10))
+      productService.getList shouldBe List(Product("Bike", 100, "Mountain bike with 24 gears", 50), Product("Car", 2000, "A powerful four door saloon", 10))
     }
 
 }
